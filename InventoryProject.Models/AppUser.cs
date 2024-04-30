adde8fd2532d6ca431e801460710c0d15cf191a5
ahmed-bs <medamed777@gmail.com>
2024-04-30 12:26:29 +0100

    added all the necessairy models : Brand,Item,User,Order etc

---
+++ InventoryProject.Models/AppUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace InventoryProject.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Adresse { get; set; }
+        public int PhoneNumber { get; set; }
+        
+    }
+}
